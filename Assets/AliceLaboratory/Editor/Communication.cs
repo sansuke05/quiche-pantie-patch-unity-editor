e6697b5e17b704b3a68a9c71c4aaa096e794909d
Takanori Watanabe <takanori5064@gmail.com>
2019-09-13 15:12:22 +0900

    [Update]パンツ一覧の取得、データのパース処理を追加

--- Assets/AliceLaboratory/Editor/Communication.cs
+++ Assets/AliceLaboratory/Editor/Communication.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using AliceLaboratory.Editor;
 using UnityEngine.Networking;
 
 public class Communication {
-	private const string URL = "http://pantie-patch.herokuapp.com/api/dream/0001.png";
+	private const string DREAMS_BASE_URL = "http://pantie-patch.herokuapp.com/api/dream/";
 
 	private WWW www;
 	
 	FileCreator creator;
 
 	public Communication() {
-		www = new WWW(URL);
+		www = new WWW(DREAMS_BASE_URL);
+	}
+
+	public string GetDreams() {
+		Dream dream = null;
+		
+		www.MoveNext();
+		
+		// リクエストが完了した時の処理
+		if (www.isDone) {
+			dream = JsonUtility.FromJson<Dream>(www.text);
+		}
+
+		if (dream != null) {
+			foreach (var image in dream.images) {
+				www = new WWW(DREAMS_BASE_URL + image);
+				// ここの続き
+			}
+		}
+		return "";
 	}
 
 	public string GetTexture() {
