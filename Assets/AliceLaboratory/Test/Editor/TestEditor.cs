e266ae86b2fa0c96a7eaac26ecf3a72d5af6338a
Takanori Watanabe <takanori5064@gmail.com>
2019-10-22 16:33:17 +0900

    [add]Texture2Texture2Dのテストコードを追加

--- Assets/AliceLaboratory/Test/Editor/TestEditor.cs
+++ Assets/AliceLaboratory/Test/Editor/TestEditor.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 namespace AliceLaboratory.Editor {
     public class TestEditor {
-        //[MenuItem( "Tools/Example" )]
+        private const string BASE_PATH = "Test/";
+
+        [MenuItem( "Test/Execution" )]
         private static void Example() {
             var type = typeof( GameObject );
             var content = EditorGUIUtility.ObjectContent( null, type );
             var image = content.image;
             var tex = Convert(image);
-            var png = tex.EncodeToPNG();
-            File.WriteAllBytes( "result.png", png );
+            var f = new FilerOperator();
+            f.Create("ressult.png", BASE_PATH + "Results", tex);
+            Debug.Log("Test is Done");
         }
 
         public static Texture2D Convert(Texture texture) {
