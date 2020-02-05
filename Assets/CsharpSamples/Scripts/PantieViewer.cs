f41a4f64cd35311271cc14c05ffcc95fa1de725d
Takanori Watanabe <takanori5064@gmail.com>
2020-02-06 01:08:48 +0900

    [update]技術書用のサンプル追加

--- Assets/CsharpSamples/Scripts/PantieViewer.cs
+++ Assets/CsharpSamples/Scripts/PantieViewer.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class PantieViewer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		// コンポネントを型として使う
-		
+        string dreamDir = "Assets/AliceLaboratory/Dreams/";
+        string path = dreamDir + "0001.png";
+
+        Texture2D tex = new Texture2D(0, 0);
+        tex.LoadImage(File.ReadAllBytes(path));
+
+        Renderer r = GetComponent<Renderer>();
+        r.material.mainTexture = tex;
 	}
 	
 	// Update is called once per frame
