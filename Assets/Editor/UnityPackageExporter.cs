dc9619ddef9ccca62d1761d24728a945c5a17755
sansuke05 <takanori5064@gmail.com>
2021-02-14 16:14:50 +0900

    fix create unitypackage workflow
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix create unitypackage workflow
    
    fix create unitypackage workflow
    
    fix create unitypackage workflow
    
    fix create unitypackage workflow
    
    fix create unitypackage workflow
    
    fix create unitypackage workflow
    
    fix create unitypackage workflow
    
    fix create unitypackage workflow
    
    fix create unitypackage workflow
    
    update workflow

---
+++ Assets/Editor/UnityPackageExporter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+
+public static class UnityPackageExporter
+{
+    [MenuItem("Tools/Export Unitypackage")]
+    public static void Export()
+    {
+        var exportDirs = new []
+        {
+            "Assets/AliceLaboratory/Editor",
+            "Assets/Plugins/UniTask"
+        };
+        var exportPath = "./QuichePantiePatchEditor.unitypackage";
+
+        AssetDatabase.ExportPackage(exportDirs, exportPath, ExportPackageOptions.Recurse);        
+    }
+}
