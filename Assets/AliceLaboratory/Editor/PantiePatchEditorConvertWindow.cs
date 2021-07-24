1d38aeea2c9748ee7532717f3f0a45f5a84edb8a
Takanori Watanabe <takanori5064@gmail.com>
2021-07-24 20:04:32 +0900

    upgrade 2019 & fix some bugs

--- Assets/AliceLaboratory/Editor/PantiePatchEditorConvertWindow.cs
+++ Assets/AliceLaboratory/Editor/PantiePatchEditorConvertWindow.cs
@@ -1,67 +1,52 @@
 using System;
-using UnityEditor;
-using UnityEngine;
 using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Linq;
-
-#if UNITY_2019_1_OR_NEWER
+using UnityEditor;
 using UnityEditor.UIElements;
+using UnityEngine;
+#if UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
+
 #else
 using UnityEngine.Experimental.UIElements;
 #endif
 
-
-namespace AliceLaboratory.Editor {
-    public class PantiePatchEditorConvertWindow : EditorWindow {
-
-#pragma warning disable CS0414//The field 'PantiePatchEditorConvertWindow.visualTree' is assigned but its value is never used
-        [SerializeField] VisualTreeAsset visualTree = default;
-#pragma warning restore
-
+namespace AliceLaboratory.Editor
+{
+    public sealed class PantiePatchEditorConvertWindow : EditorWindow
+    {
         AsyncReactiveProperty<Texture> BaseAvatarTexture { get; } = new AsyncReactiveProperty<Texture>(null);
         AsyncReactiveProperty<Texture> ConvertTexture { get; } = new AsyncReactiveProperty<Texture>(null);
         AsyncReactiveProperty<int> SelectedAvatarIndex { get; } = new AsyncReactiveProperty<int>(-1);
         AsyncReactiveProperty<bool> ConvertRunning { get; } = new AsyncReactiveProperty<bool>(false);
+        ReadOnlyAsyncReactiveProperty<bool> CanStartConvert { get; }
 
-        ReadOnlyAsyncReactiveProperty<bool> CanStartConvert { get; } 
-        
+        Vector2 _scrollPosition = Vector2.zero;
+        AvatarsData _avatarsData;
+
         public PantiePatchEditorConvertWindow()
         {
             CanStartConvert = UniTaskAsyncEnumerable.CombineLatest(ConvertTexture, SelectedAvatarIndex, ConvertRunning,
-               (convertTexture, selectedAvatarIndex, convertRunning) => convertTexture != null && selectedAvatarIndex != -1 && !convertRunning).ToReadOnlyAsyncReactiveProperty(default);
+                    (convertTexture, selectedAvatarIndex, convertRunning) =>
+                        convertTexture != null && selectedAvatarIndex != -1 && !convertRunning)
+                .ToReadOnlyAsyncReactiveProperty(default);
         }
-        //スクロール位置
-        private Vector2 _scrollPosition = Vector2.zero;
 
-        private AvatarsData _avatarsData;
-
-        /// <summary>
-        /// Initialization
-        /// </summary>
         [MenuItem("Editor/PantiePatch/パンツ変換")]
-        private static void Init() {
-            
-            GetWindow<PantiePatchEditorConvertWindow>("パンツ変換").Show();
+        static void Initialize()
+        {
+            var window = GetWindow<PantiePatchEditorConvertWindow>();
+            window.titleContent = new GUIContent("パンツ変換");
         }
-        
-        #region Unity Method
-        
-        private void OnEnable() {
+
+        void OnEnable()
+        {
             // ScriptableObjectからアバターデータを読み込む
             var file = new FilerOperator();
             _avatarsData = file.readAvatersData();
-
-           
-#if UNITY_2019_4_OR_NEWER
-            CreateGUI();
-#endif
-
         }
 
 #if !UNITY_2019_4_OR_NEWER
-
-
         /// <summary>
         /// GUI setting
         /// </summary>
@@ -70,18 +55,21 @@ namespace AliceLaboratory.Editor {
                 using (new GUILayout.HorizontalScope()) {
                     EditorGUILayout.LabelField("変換するパンツを選択");
                     var option = new []{GUILayout.Width (64), GUILayout.Height (64)};
-                    ConvertTexture.Value = EditorGUILayout.ObjectField(ConvertTexture.Value, typeof(Texture), false, option) as Texture;
+                    ConvertTexture.Value =
+ EditorGUILayout.ObjectField(ConvertTexture.Value, typeof(Texture), false, option) as Texture;
                 }
                 using (new GUILayout.HorizontalScope()) {
                     EditorGUILayout.LabelField("重ねるアバターのテクスチャを選択");
                     var option = new []{GUILayout.Width (64), GUILayout.Height (64)};
-                    BaseAvatarTexture.Value = EditorGUILayout.ObjectField(BaseAvatarTexture.Value, typeof(Texture), false, option) as Texture;
+                    BaseAvatarTexture.Value =
+ EditorGUILayout.ObjectField(BaseAvatarTexture.Value, typeof(Texture), false, option) as Texture;
                 }
                 EditorGUILayout.LabelField("変換対象のアバター");
             }
             if (_avatarsData != null) {
                 _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
-                SelectedAvatarIndex.Value = GUILayout.SelectionGrid(SelectedAvatarIndex.Value, _avatarsData.display_names, 2);
+                SelectedAvatarIndex.Value =
+ GUILayout.SelectionGrid(SelectedAvatarIndex.Value, _avatarsData.display_names, 2);
                 EditorGUILayout.EndScrollView();
             } else {
                 EditorGUILayout.HelpBox(
@@ -98,29 +86,23 @@ namespace AliceLaboratory.Editor {
                 EditorGUI.EndDisabledGroup();
             }
         }
-#endif
-        #endregion
-
-#if UNITY_2019_4_OR_NEWER
-
-
+#else
         void CreateGUI()
-         {
-
+        {
+            const string layoutFilePath = "Assets/AliceLaboratory/Editor/Layout/PantiePatchEditorConvertWindow.uxml";
             var root = rootVisualElement;
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(layoutFilePath);
             visualTree.CloneTree(root);
-            
 
-        
-            
             var convertTextureSelector = root.Q<ObjectField>("ConvertTextureSelector");
             convertTextureSelector.objectType = typeof(Texture);
-
-            convertTextureSelector.RegisterValueChangedCallback((change) => ConvertTexture.Value = (Texture)change.newValue);
+            convertTextureSelector.RegisterValueChangedCallback(change =>
+                ConvertTexture.Value = (Texture) change.newValue);
 
             var baseAvatarTextureSelector = root.Q<ObjectField>("BaseAvatarTextureSelector");
             baseAvatarTextureSelector.objectType = typeof(Texture);
-            baseAvatarTextureSelector.RegisterValueChangedCallback((change) => BaseAvatarTexture.Value = (Texture)change.newValue);
+            baseAvatarTextureSelector.RegisterValueChangedCallback(change =>
+                BaseAvatarTexture.Value = (Texture) change.newValue);
 
             var avatarsView = root.Q<IMGUIContainer>();
             avatarsView.onGUIHandler = () =>
@@ -128,7 +110,8 @@ namespace AliceLaboratory.Editor {
                 if (_avatarsData != null)
                 {
                     _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
-                    SelectedAvatarIndex.Value = GUILayout.SelectionGrid(SelectedAvatarIndex.Value, _avatarsData.display_names, 2);
+                    SelectedAvatarIndex.Value =
+                        GUILayout.SelectionGrid(SelectedAvatarIndex.Value, _avatarsData.display_names, 2);
                     EditorGUILayout.EndScrollView();
                 }
                 else
@@ -142,23 +125,16 @@ namespace AliceLaboratory.Editor {
 
             var convertButton = root.Q<Button>();
             convertButton.SetEnabled(false);
-
-            CanStartConvert.ForEachAsync((canStartConvert) => convertButton.SetEnabled(canStartConvert));
-
-            
-
-           
-            convertButton.clicked += Convert(ConvertTexture.Value, BaseAvatarTexture.Value, SelectedModelName);
-
-        
-
+            CanStartConvert.ForEachAsync(canStartConvert => convertButton.SetEnabled(canStartConvert));
+            convertButton.clicked += () => Convert(ConvertTexture.Value, BaseAvatarTexture.Value, SelectedModelName);
         }
 #endif
         string SelectedModelName => _avatarsData.models[SelectedAvatarIndex.Value];
 
-        void Convert(Texture convertTexture, Texture baseAvatarTexture, string modelName) => ConvertImpl(convertTexture, baseAvatarTexture, modelName).Forget();
+        void Convert(Texture convertTexture, Texture baseAvatarTexture, string modelName) =>
+            ConvertImpl(convertTexture, baseAvatarTexture, modelName).Forget();
 
-        private async UniTaskVoid ConvertImpl(Texture convertTexture,Texture baseAvatarTexture,string modelName) 
+        async UniTaskVoid ConvertImpl(Texture convertTexture, Texture baseAvatarTexture, string modelName)
         {
             ConvertRunning.Value = true;
             try
@@ -197,6 +173,5 @@ namespace AliceLaboratory.Editor {
                 ConvertRunning.Value = false;
             }
         }
-
     }
 }
